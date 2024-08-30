f17e613777fafeb5af65cc81cf082bf515c85aa1
Grant Hamm <ghamm@nicmapvision.com>
2024-08-30 06:31:17 -0400

    addtl tests, version bump, readme

--- Anthropic.SDK.Tests/CacheControlTests.cs
+++ Anthropic.SDK.Tests/CacheControlTests.cs
@@ -443,6 +443,100 @@ namespace Anthropic.SDK.Tests
 
             Debug.WriteLine(res.Message);
         }
+
+        [TestMethod]
+        public async Task TestCacheControlClaude3MessageWithNotEnoughToCache()
+        {
+            var client = new AnthropicClient();
+            var messages = new List<Message>();
+            messages.Add(new Message(RoleType.User, "Write me a sonnet about the Statue of Liberty",
+                new CacheControl() { Type = CacheControlType.ephemeral }));
+            var parameters = new MessageParameters()
+            {
+                Messages = messages,
+                MaxTokens = 512,
+                Model = AnthropicModels.Claude35Sonnet,
+                Stream = false,
+                Temperature = 1.0m,
+                PromptCaching = PromptCacheType.FineGrained
+            };
+            var res = await client.Messages.GetClaudeMessageAsync(parameters);
+            Assert.IsNotNull(res.Message.ToString());
+            Assert.IsTrue(res.Usage.CacheCreationInputTokens == 0);
+        }
+
+        [TestMethod]
+        public async Task TestCacheControlOfAssistantMessages()
+        {
+            string resourceName = "Anthropic.SDK.Tests.BillyBudd.txt";
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            await using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new StreamReader(stream);
+            string content = await reader.ReadToEndAsync();
+
+
+
+
+            var client = new AnthropicClient();
+            var messages = new List<Message>()
+            {
+                new Message(RoleType.User, "What are the key literary themes of this novel?"),
+            };
+            var systemMessages = new List<SystemMessage>()
+            {
+                new SystemMessage("You are an expert at analyzing literary texts."),
+                new SystemMessage(content, new CacheControl() { Type = CacheControlType.ephemeral })
+            };
+            var parameters = new MessageParameters()
+            {
+                Messages = messages,
+                MaxTokens = 1024,
+                Model = AnthropicModels.Claude35Sonnet,
+                Stream = false,
+                Temperature = 0m,
+                System = systemMessages,
+                PromptCaching = PromptCacheType.FineGrained
+            };
+            var res = await client.Messages.GetClaudeMessageAsync(parameters);
+
+            Debug.WriteLine(res.Message);
+            Assert.IsTrue(res.Usage.CacheCreationInputTokens > 0 || res.Usage.CacheReadInputTokens > 0);
+
+            //try caching an assistant message
+            res.Message.Content.First().CacheControl = new CacheControl() { Type = CacheControlType.ephemeral };
+
+            messages.Add(res.Message);
+            messages.Add(new Message(RoleType.User, "Who is the main character and how old is he?"));
+
+            var res2 = await client.Messages.GetClaudeMessageAsync(parameters);
+
+            Assert.IsTrue(res2.Usage.CacheReadInputTokens > 0);
+
+            Assert.IsNotNull(res2.Message.ToString());
+
+            //message 3
+            messages.Add(res2.Message);
+            messages.Add(new Message(RoleType.User, "Who is the main antagonist and how old is he?"));
+
+            var res3 = await client.Messages.GetClaudeMessageAsync(parameters);
+
+            Assert.IsTrue(res3.Usage.CacheReadInputTokens > 0);
+
+            Assert.IsNotNull(res3.Message.ToString());
+
+            //message 4
+            messages.Add(res3.Message);
+            messages.Add(new Message(RoleType.User, "What year is the book set in?"));
+
+            var res4 = await client.Messages.GetClaudeMessageAsync(parameters);
+
+            Assert.IsTrue(res4.Usage.CacheReadInputTokens > 0);
+
+            Assert.IsNotNull(res4.Message.ToString());
+
+        }
     }
 
     
